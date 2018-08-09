6b436f1033eaab69e28bf05918c4596986009c49
ALAxHxC <jenizaro123>
2018-08-08 22:09:36 -0500

    Agregando servicios de pacientes

---
+++ app/src/main/java/ponny/org/monitora/models/monitora/servicios/ApiMonitoraPacientes.java
@@ -0,0 +1,9 @@
+package ponny.org.monitora.models.monitora.servicios;
+
+import android.content.Context;
+
+public class ApiMonitoraPacientes  extends ApiMonitora {
+    public ApiMonitoraPacientes(Context context) {
+        super(context);
+    }
+}
